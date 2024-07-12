bb8aa57cc86b873709f2aa98dc91998ff8af531f
martinhms <martinhms2015@gmail.com>
2024-07-12 10:42:47 -0300

    [COMPASS-SCREEN] - Added UI screen (#1)
    Co-authored-by: Martin Horacio Mendez <martinhmendez@MBP-de-Martin.fibertel.com.ar>

--- app/src/main/java/com/example/compassapp/MainActivity.kt
+++ app/src/main/java/com/example/compassapp/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compassapp.ui.theme.CompassAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,53 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CompassAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CompassScreen(
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CompassScreen() {
+    Column(
+        modifier = Modifier
+            .wrapContentHeight()
+            .fillMaxWidth().padding(top = 50.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Results",
+            modifier = Modifier
+                .padding(20.dp)
+                .wrapContentHeight()
+        )
+
+        TextField(
+            value = "Enter text", onValueChange = { }, singleLine = false,
+            maxLines = 10,
+            modifier = Modifier
+                .padding(20.dp)
+                .wrapContentHeight()
+                .fillMaxWidth()
+        )
+        Button(
+            onClick = {
+                // onTaskAdded(task)
+                //  task = ""
+            }, modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
+            Text(text = "Run requests")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CompassAppTheme {
-        Greeting("Android")
+        CompassScreen()
     }
 }
